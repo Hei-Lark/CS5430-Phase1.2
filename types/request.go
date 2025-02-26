e9682c674c48d9186b6c67a71664b02a6d3ada6a
Heilong-Lark <54684355+Heilong-Lark@users.noreply.github.com>
2025-02-25 22:30:32 -0500

    Setup

--- types/request.go
+++ types/request.go
@@ -20,4 +20,5 @@ type Request struct {
 	Src_key string      `json:"src_key"`
 	Dst_key string      `json:"dst_key"`
 	UID     string      `json:"uid"`
+	Nonce   string      `json:"nonce"`
 }
--- types/response.go
+++ types/response.go
@@ -11,4 +11,5 @@ type Response struct {
 	Status Code        `json:"status"`
 	Val    interface{} `json:"val"`
 	UID    string      `json:"uid"`
+	Nonce  string      `json:"nonce"`
 }
